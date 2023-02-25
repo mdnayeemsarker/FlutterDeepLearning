8d2255f756f81378aa39a12006d23cec44c94080
Md Nayeem Asha <nayeemasha703@gmail.com>
2023-02-25 16:52:37 +0600

    Functions, Classes, Routes, initialRoutes, Theme & Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dl/pages/home_page.dart';
+import 'package:flutter_dl/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
-      darkTheme: ThemeData(brightness: Brightness.dark
-          // primarySwatch: Colors.blueGrey
-          ),
-    );
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.red),
+        darkTheme: ThemeData(brightness: Brightness.dark
+            // primarySwatch: Colors.blueGrey
+            ),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+        });
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+              fontSize: 40,
+              color: Colors.blueAccent),
+        ),
+      ),
+    );
+  }
+}
