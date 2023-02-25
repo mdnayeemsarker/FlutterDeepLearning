723e92973a5334c4af46f4218492fc2e048acd67
Md Nayeem Asha <nayeemasha703@gmail.com>
2023-02-25 18:11:22 +0600

    Adding Images, Google Fonts | ElevatedButton | Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dl/pages/home_page.dart';
 import 'package:flutter_dl/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.red),
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          fontFamily: GoogleFonts.lato().fontFamily,
+        ),
         darkTheme: ThemeData(brightness: Brightness.dark
             // primarySwatch: Colors.blueGrey
             ),
-        initialRoute: "/home",
         routes: {
           "/": (context) => LoginPage(),
           "/home": (context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,13 +4,46 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40,
-              color: Colors.blueAccent),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png"),
+          SizedBox(height: 30),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "UserName",
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hi Flutter Developer Nayeem");
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
