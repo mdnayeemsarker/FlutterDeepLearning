9762291c746ee04fe3937f4fdd96af76a68f253f
Md Nayeem Asha <nayeemasha703@gmail.com>
2023-02-26 20:06:21 +0600

    Local Files | Load & Decode JSON && JSON Mapping | Data Class Generator | ProgressIndicator

--- lib/model/products_list.dart
+++ lib/model/products_list.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class ProductsModel {
-  static final items = [
+  static List<Item> items = [
     Item(
       1,
       "iPhone 9",
@@ -38,4 +41,71 @@ class Item {
       this.brand,
       this.category,
       this.thumbnail);
+
+  Item copyWith({
+    int? id,
+    String? title,
+    String? description,
+    num? price,
+    num? discountPercentage,
+    num? rating,
+    num? stock,
+    String? brand,
+    String? category,
+    String? thumbnail,
+  }) {
+    return Item(
+      id ?? this.id,
+      title ?? this.title,
+      description ?? this.description,
+      price ?? this.price,
+      discountPercentage ?? this.discountPercentage,
+      rating ?? this.rating,
+      stock ?? this.stock,
+      brand ?? this.brand,
+      category ?? this.category,
+      thumbnail ?? this.thumbnail,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'price': price,
+      'discountPercentage': discountPercentage,
+      'rating': rating,
+      'stock': stock,
+      'brand': brand,
+      'category': category,
+      'thumbnail': thumbnail,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      map['id'] as int,
+      map['title'] as String,
+      map['description'] as String,
+      map['price'] as num,
+      map['discountPercentage'] as num,
+      map['rating'] as num,
+      map['stock'] as num,
+      map['brand'] as String,
+      map['category'] as String,
+      map['thumbnail'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, title: $title, description: $description, price: $price, discountPercentage: $discountPercentage, rating: $rating, stock: $stock, brand: $brand, category: $category, thumbnail: $thumbnail)';
+  }
+
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,31 +1,64 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dl/model/products_list.dart';
 import 'package:flutter_dl/widgets/deawer.dart';
 import 'package:flutter_dl/widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   String name = "MD. NAYEEM SARKER";
+
   int age = 24;
+
   int mobile = 01714466703;
+
+  @override
+  void initState() {
+    super.initState();
+    lodeData();
+  }
+
+  lodeData() async {
+    final productsJson =
+        await rootBundle.loadString("assets/files/products.json");
+    final decodedData = jsonDecode(productsJson);
+    var productsListData = decodedData["products"];
+    ProductsModel.items = List.from(productsListData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(5, (index) => ProductsModel.items[0]);
+    // final dummyList = List.generate(5, (index) => ProductsModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("App Name"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          // itemCount: ProductsModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidgets(
-              item: dummyList[index],
-              // item: ProductsModel.items[index],
-            );
-          },
-        ),
+        child: (ProductsModel.items.isNotEmpty)
+            ? ListView.builder(
+                // itemCount: dummyList.length,
+                itemCount: ProductsModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidgets(
+                    // item: dummyList[index],
+                    item: ProductsModel.items[index],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                color: Colors.red,
+              )),
       ),
       drawer: MyDrawer(),
     );
